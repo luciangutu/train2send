ff4c62dc2718290cf0eadeaf393e23ef261e8487
Lucian Gutu <luciangutu@gmail.com>
2026-09-10 16:43:35 +0300

    Release 1.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.luciangutu.train2send"
         minSdk = 26
         targetSdk = 37
-        versionCode = 32
-        versionName = "1.6.2"
+        versionCode = 33
+        versionName = "1.6.3"
 
         ndk {
             debugSymbolLevel = "FULL"
